dbb5bc52492a9ae0112193705ce048a40ef7b012
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-04-14 14:04:07 +0530

    Add files via upload

---
+++ Day 9/Polymorphism/Polymorphism/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Rectangle:shape
+    {
+        public override void Drawshape()
+        {
+            Console.WriteLine("Draw Rectangle");
+        }
+    }
+}
