43ab9e63d50184d9afe972ddd3d7d7a453081b99
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-04-24 09:49:43 +0530

    Add files via upload

---
+++ Day 9/AccountApp/AccountApp/Program.cs
@@ -0,0 +1,112 @@
+﻿
+using System;
+
+namespace AccountApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ch;
+            Account a = null;
+
+            do
+            {
+
+                Console.WriteLine("Account application");
+                Console.WriteLine("1. Create Account");
+                Console.WriteLine("2. Display account details");
+                Console.WriteLine("3. Deposit  amount");
+                Console.WriteLine("4.withdraw amount");
+                Console.WriteLine("5.Balance amount");
+                Console.WriteLine("0. Quit");
+                Console.WriteLine("Enter your choice");
+
+                ch = int.Parse(Console.ReadLine());
+
+                switch (ch)
+                {
+                    case 1:
+
+                        Console.WriteLine("Enter your account id");
+                        int id = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter your name");
+                        string _name = Console.ReadLine();
+                        Console.WriteLine("opening balance");
+                        decimal _balance = decimal.Parse(Console.ReadLine());
+                        a = new Account();
+                        a.createaccount(id, _name, _balance);
+                        break;
+
+                    case 2:
+                        if (a != null)
+                        {
+                            a.displayinfo();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Create an account first");
+
+                        }
+                        break;
+
+                    case 3:
+
+                        if (a != null)
+                        {
+                            a.Deposit(5000);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Create an account first");
+
+                        }
+                        break;
+                    case 4:
+
+                        if (a != null)
+                        {
+                            a.Withdraw(500);
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Create an account first");
+
+                        }
+                        break;
+                    case 5:
+                        if (a != null)
+                        {
+                            a.CheckBalance();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Create an account first");
+
+                        }
+
+                        break;
+                    case 0:
+
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Wrong choice");
+                        break;
+
+                }
+                //{
+                //Console.WriteLine("Thanks for using bank account");
+                //}
+            } while (ch != 0);
+            {
+                Console.WriteLine("Thanks for using bank account");
+            }
+
+
+        }
+    }
+}
---
+++ Day 9/Figure/Figure/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure fig= new square();
+            fig.Dimension = 10;
+            Console.WriteLine(fig.Area());
+            Console.WriteLine(fig.Perimeter());
+
+        }
+        abstract class Figure
+        {
+            public int Dimension;
+            public abstract double Area();
+            public abstract double Perimeter();
+        }
+        class square : Figure
+        {
+            public override double Area()
+            {
+                return Dimension * Dimension;
+            }
+            public override double Perimeter()
+            {
+                return 4* Dimension;
+            }
+
+        }
+        class circle : Figure
+        {
+            public override double Area()
+            {
+                return Math.PI * Dimension * Dimension;
+            }
+            public override double Perimeter()
+            {
+                return 2 * Math.PI * Dimension;
+            }
+        }
+    }
+}
