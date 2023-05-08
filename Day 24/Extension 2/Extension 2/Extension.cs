6eb5de5217229b4814a63d910ae17b26014ba22f
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-05-08 10:12:42 +0530

    Add files via upload

---
+++ Day 24/Extension 2/Extension 2/Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_2
+{
+    public static class Extension
+    {
+        public static string Concat(this string a,string b )
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Day 24/Extension 2/Extension 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string i="keerthana";
+            string j= "karthik";
+            string result = i.Concat(j);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
