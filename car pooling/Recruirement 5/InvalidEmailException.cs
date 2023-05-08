5ffcfaf2c6de3b7016054635c0bd4e2e009e6e98
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-05-08 09:31:04 +0530

    Add files via upload

---
+++ car pooling/Recruirement 5/InvalidEmailException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recruirement_5
+{
+    public class InvalidEmailException:Exception
+    {
+        string _email;
+        public InvalidEmailException()
+        {
+
+        }
+        public InvalidEmailException(string email)
+        {
+            _email = email;
+        }
+        public void inform()
+        {
+            Console.WriteLine("Invalid mailid");
+        }
+    }
+}
