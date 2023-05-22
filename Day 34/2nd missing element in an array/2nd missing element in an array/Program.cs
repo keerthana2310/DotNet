c093571edd100eccfe05ab5b0fb3dc4179fd27e2
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-05-22 12:04:15 +0530

    Add files via upload

---
+++ Day 34/2nd missing element in an array/2nd missing element in an array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2nd_missing_element_in_an_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int[] arr = { 10, 20,60, 30, 50 };
+            Array.Sort(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+              //  Console.WriteLine(arr[i]);
+                n = arr[i + 1] - arr[i];
+                if (n > 1)
+                {
+                    Console.WriteLine(arr[i] + 1);
+                    break;
+                }
+            }
+
+           
+
+        }
+    }
+}
---
+++ Day 34/array delete/array delete/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_delete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]  arr = new int[] { 1, 2, 3 };
+             
+        }
+    }
+}
