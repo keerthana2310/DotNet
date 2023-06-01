f783926c0a6d9cee5a9f7a8af5538e497c18b1cc
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-06-01 17:23:47 +0530

    Add files via upload

---
+++ Day 43/main.js
@@ -0,0 +1,23 @@
+// var a=document.getElementsByName("text")[0];
+// //console.log(a);
+// var b=document.getElementById("head");
+// function message(){
+//     b.innerHTML="Hello"+a.value;
+// }
+
+
+// var a=document.querySelectorAll("h1");
+// console.log(a);
+// a[0].innerHTML="learn java";
+// a[1].style.color="red";
+
+
+// var a=document.querySelectorAll(".hclass");
+// a[0].style.colour = "red";
+// a[1].innerHTML="learn java";
+
+var heading=document.createElement("h1");
+function create(){
+    heading.innerHTML="Hello World";
+    document.body.appendchild(heading);
+}
