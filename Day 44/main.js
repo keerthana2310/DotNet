fe44c3dd423950a1b2a9ad8289adf1ee532a8bde
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-06-01 17:27:18 +0530

    Add files via upload

---
+++ Day 44/main.js
@@ -0,0 +1,27 @@
+// document.getElementById("demo").innerHTML=5+6;
+// document.write(9+7);
+// // window.alert(8+9);
+// // alert(2);
+// console.log(3);
+// let x,y,z;
+// x=2;
+// y=3;
+// z=x+y;
+// document.getElementById("demo").innerHTML="The value of z is "+ z;
+// let a,b,c;
+// a=3;
+// b=1;
+// c=a+b;
+// document.getElementById("demo").innerHTML=c;
+// function myFunction(){
+//     document.getElementById("demo1").innerHTML="Hello";
+//     document.getElementById("demo2").innerHTML="Karthik";
+
+//}
+// let car="Seltos";
+// document.getElementById("demo").innerHTML=car;
+const cars=["sonet,seltos","fortuner"];
+cars[0]="Endaevour";
+cars.push("LandCruizer");
+document.getElementById("demo").innerHTML=cars;
+
