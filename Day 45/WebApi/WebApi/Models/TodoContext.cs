fed672eccf177595f5112fbbc3e390649327d8e5
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-06-01 17:38:46 +0530

    Add files via upload

---
+++ Day 45/WebApi/WebApi/Models/TodoContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApi.Models
+{
+    
+    using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace WebApi.Models
+    {
+        public class TodoContext : DbContext
+        {
+            public TodoContext(DbContextOptions<TodoContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<TodoItem> TodoItems { get; set; } = null!;
+        }
+    }
+}
