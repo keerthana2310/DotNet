d7798d07514906fe5143a76e7ee46a072c8426ef
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-06-08 16:49:06 +0530

    Add files via upload

---
+++ Day 48/ProductMicroservice/ProductMicroservice/Repository/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProductMicroservice.Model;
+
+namespace ProductMicroservice.Repository
+{
+    interface IProductRepository
+    {
+        IEnumerable<Product> GetProducts();
+
+        Product GetProductByID(int id);
+
+
+        void InsertProduct (Product Product);
+       
+        void DeleteProduct (int ProductId);
+        void UpdateProduct(Product Product);
+        
+        void Save();
+
+    }
+}
