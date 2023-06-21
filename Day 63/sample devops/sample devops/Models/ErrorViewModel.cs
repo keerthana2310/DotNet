7b23c3c0efc1e03717dc3b50d6a6524395a1bcb7
keerthana2310 <129931458+keerthana2310@users.noreply.github.com>
2023-06-21 10:17:53 +0530

    Add files via upload

---
+++ Day 63/sample devops/sample devops/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sample_devops.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
